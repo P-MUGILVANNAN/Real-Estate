26a635f894014eaab80bc1176ef2204ff8109631
Mugilvannan P <mugilvannanpichai02@gmail.com>
2024-02-16 19:38:22 +0530

    create controller

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
-const express = require("express");
-const mongoose=require("mongoose");
-const dotenv=require("dotenv");
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js"
+
 dotenv.config();
 
 //mongodb connection
@@ -11,3 +13,5 @@ const app=express();
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
+
+app.use("/server/user",userRouter);
