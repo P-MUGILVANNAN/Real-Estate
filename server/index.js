d97cbe1c411ccced8598425316f86b0cda50d5b3
Mugilvannan P <mugilvannanpichai02@gmail.com>
2024-02-16 19:09:01 +0530

    mongodb connect

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const mongoose=require("mongoose");
+const dotenv=require("dotenv");
+dotenv.config();
+
+//mongodb connection
+mongoose.connect(process.env.MONGO).then(()=>{console.log('Mongodb connected')}).catch((err)=> console.log(err))
 
 const app=express();
 
